8ecfc144a46810538a5ce37967dfccab2118d178
Soungmin Ryu <rolrob25@gmail.com>
2025-08-20 16:01:24 +0900

    787

---
+++ problems/787. Cheapest Flights Within K Stops/solution.cpp
@@ -0,0 +1,30 @@
+// 787. Cheapest Flights Within K Stops / solution.cpp
+// Time: O(K * M)  |  Space: O(N)
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    int findCheapestPrice(int n, vector<vector<int>>& flights, int src, int dst, int k) {
+        const int INF = 1e9;
+        vector<int> dist(n, INF);
+        dist[src] = 0;
+
+        for (int i = 0; i <= k; ++i) 
+        {
+            vector<int> ndist = dist;
+            for (const auto& f : flights) 
+            {
+                int u = f[0], v = f[1], w = f[2];
+                if (dist[u] == INF) continue;
+                if (ndist[v] > dist[u] + w) 
+                {
+                    ndist[v] = dist[u] + w;
+                }
+            }
+            dist.swap(ndist);
+        }
+
+        return dist[dst] == INF ? -1 : dist[dst];
+    }
+};
